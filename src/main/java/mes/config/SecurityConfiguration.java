1caa4d6652f7e4f8c64ac6cc5906075df7f63b16
DESKTOP-TG6HNLE\Act-PC-11_yuri <kyl1563@naver.com>
2026-07-24 18:10:24 +0900

    2026.07.24 - 김유리 시큐리티추가

--- src/main/java/mes/config/SecurityConfiguration.java
+++ src/main/java/mes/config/SecurityConfiguration.java
@@ -88,7 +88,9 @@ public class SecurityConfiguration {
                                 // API 및 외부 연동
                                 "/useridchk/**", "/user-auth/**", "/user-auth/save",
                                 "/popbill/webhook", "/api/transaction/input/**",
-                                "/api/das_device", "/authentication/**", "/api/common/**"
+                                "/api/das_device", "/authentication/**", "/api/common/**",
+																// 전자결재
+																"/appkey/**"
                         ).permitAll()
                         .antMatchers("/setup").hasAuthority("admin")
                         .anyRequest().authenticated()
